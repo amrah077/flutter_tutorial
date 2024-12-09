4789907759a9ee8ff7ba81c843cdc0ef6433a64a
amrah072207 <fathimaamrahimran@gmail.com>
2024-12-09 15:16:09 +0530

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SettingsPage(),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Settings',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'Rubik'
+            
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Account Settings',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          const SettingsRow(title: 'Edit profile'),
+          const SettingsRow(title: 'Change password'),
+          const SettingsRow(title: 'Change username'),
+          const SizedBox(height: 16), // SPACEEEEEEE
+
+          // DIVIDERRRRRRRRRRR
+          const Divider(height: 1, thickness: 0.5, color: Colors.grey),
+
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'More',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          const SettingsRow(title: 'Push Notifications'),
+          const SettingsRow(title: 'About us'),
+          const SettingsRow(title: 'Privacy Policy'),
+        ],
+      ),
+    );
+  }
+}
+
+class SettingsRow extends StatelessWidget {
+  final String title;
+
+  const SettingsRow({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 16),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom:12.0),
+            child: const Icon(Icons.chevron_right, color: Colors.grey),
+          ),
+        ],
+      ),
+    );
+  }
+}
